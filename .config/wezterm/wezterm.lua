129a79426ce5723db673e5cff2d2f8ce3a34d904
r-waki <r.wakeyii@gmail.com>
2026-04-22 00:03:31 +0900

    feat()

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,35 +1,35 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- Basic Settings
 local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.use_ime = true
-config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
 
 -- Default Shell
 local function get_default_prog()
-  if wezterm.target_triple:find("windows") then
-    return { "pwsh.exe" } 
-  else
-    return { "/bin/zsh", "-l" } -- macOS/Linux
-  end
+	if wezterm.target_triple:find("windows") then
+		return { "pwsh.exe" }
+	else
+		return { "/bin/zsh", "-l" } -- macOS/Linux
+	end
 end
 config.default_prog = get_default_prog()
 
 -- Font Settings
 config.font_size = 12.0
 config.font = wezterm.font_with_fallback({
-  'HackGenNerd',
-  'HackGen35Nerd Console',
-  'BIZ UDGothic',
-  'Meiryo',
-  'MS Gothic',
-  'Noto Color Emoji',
+	"HackGenNerd",
+	"HackGen Console NF",
+	"BIZ UDGothic",
+	"Meiryo",
+	"MS Gothic",
+	"Noto Color Emoji",
 })
 
 -- Window Appearance
-config.color_scheme = 'Kasugano (terminal.sexy)'
+config.color_scheme = "Kasugano (terminal.sexy)"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
